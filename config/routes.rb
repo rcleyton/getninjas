73f2d246b1b3d2c2d6a02d9a47cb2dac7f144283
Cleyton Silva <silva.rcleyton@gmail.com>
2021-03-22 09:17:22 -0300

    Config. Redis e Sideqik

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+  
   get "dashboard", to: "dashboard#index"
   root 'dashboard#index'
   devise_for :users
